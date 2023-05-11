85bf306b142552a24d44ec69b93804dd0ba231aa
fortanelfr <fortanelfr@gmail.com>
2023-05-11 00:14:26 -0600

    Creacion repositorio

---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,38 @@
+import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { CarritoContext } from '../../context/CarritoContext'
+import { useContext } from 'react'
+
+const ItemDetail = ({ id, nombre, precio, img, stock }) => {
+
+
+  const [agregarCantidad, setAgregarCantidad] = useState(0);
+  const {agregarProducto} = useContext(CarritoContext);
+
+
+  const manejadorCantidad = (cantidad) => {
+    setAgregarCantidad(cantidad);
+    const item = {id, nombre, precio};
+    agregarProducto(item, cantidad);
+  }
+
+  return (
+    <div className='contenedorItem'>
+      <h2>Nombre: {nombre} </h2>
+      <h3>Precio: {precio} </h3>
+      <h3> ID: {id} </h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam iste reiciendis tempora unde! Quam commodi esse dicta odit nobis, enim, accusamus corrupti veritatis, excepturi aliquam laudantium? Veritatis ex sapiente deserunt.</p>
+      <img src={img} alt={nombre} />
+      {
+      }
+
+      {
+        agregarCantidad > 0 ? (<Link to="/cart"> Terminar compra </Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />)
+      }
+    </div>
+  )
+}
+
+export default ItemDetail
